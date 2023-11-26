1ea302791e21a07f152d98b309c6d277949ae011
onurozcan361 <ozcan.onur2002@gmail.com>
2023-11-26 21:07:24 +0300

    student ekleme ozelligi ve degisklik yapildiginda degisikligin ekrana yansitilmasi ozelligi geldi

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,56 +1,56 @@
-import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { AppBar, Box, Tab, Tabs } from "@mui/material";
-import { useState } from "react";
-import { StudentTab } from "./TabContexts/StudentTab";
-import { TeacherTab } from "./TabContexts/TeacherTab";
-import { CourseTab } from "./TabContexts/CourseTab";
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { AppBar, Box, Tab, Tabs } from '@mui/material';
+import { useState } from 'react';
+import StudentTab from './TabContexts/StudentTab';
+import  TeacherTab  from './TabContexts/TeacherTab';
+import CourseTab from './TabContexts/CourseTab';
 
 export const Page = () => {
-	const [value, setValue] = useState("1");
+  const [value, setValue] = useState('1');
 
-	const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-		setValue(newValue);
-	};
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
 
-	return (
-		<Box
-			sx={{
-				flexGrow: 1,
-				bgcolor: "background.paper",
-				display: "flex",
-				height: "100%",
-			}}
-		>
-			<TabContext value={value}>
-				<Tabs
-					onChange={handleChange}
-					variant="scrollable"
-					value={value}
-					orientation="vertical"
-				>
-					<Tab label="Ogrenci" value="1" />
-					<Tab label="Ogretmen" value="2" />
-					<Tab label="Ders" value="3" />
-					<Tab label="Idari Personel" value="4" />
-					<Tab label="Temizlik Gorevlileri" value="5" />
-					<Tab label="Stok" value="6" />
-					<Tab label="Gider" value="7" />
-				</Tabs>
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        bgcolor: 'background.paper',
+        display: 'flex',
+        height: '100%',
+      }}
+    >
+      <TabContext value={value}>
+        <Tabs
+          onChange={handleChange}
+          variant="scrollable"
+          value={value}
+          orientation="vertical"
+        >
+          <Tab label="Ogrenci" value="1" />
+          <Tab label="Ogretmen" value="2" />
+          <Tab label="Ders" value="3" />
+          <Tab label="Idari Personel" value="4" />
+          <Tab label="Temizlik Gorevlileri" value="5" />
+          <Tab label="Stok" value="6" />
+          <Tab label="Gider" value="7" />
+        </Tabs>
 
-				<TabPanel value="1">
-					<StudentTab></StudentTab>
-				</TabPanel>
-				<TabPanel value="2">
-					<TeacherTab></TeacherTab>
-				</TabPanel>
-				<TabPanel value="3">
-					<CourseTab></CourseTab>
-				</TabPanel>
-				<TabPanel value="4">Idari Personeller</TabPanel>
-				<TabPanel value="5">Temizlik Gorevlileri</TabPanel>
-				<TabPanel value="6">Stoklar</TabPanel>
-				<TabPanel value="7">Giderler</TabPanel>
-			</TabContext>
-		</Box>
-	);
+        <TabPanel value="1">
+          <StudentTab></StudentTab>
+        </TabPanel>
+        <TabPanel value="2">
+          <TeacherTab></TeacherTab>
+        </TabPanel>
+        <TabPanel value="3">
+          <CourseTab></CourseTab>
+        </TabPanel>
+        <TabPanel value="4">Idari Personeller</TabPanel>
+        <TabPanel value="5">Temizlik Gorevlileri</TabPanel>
+        <TabPanel value="6">Stoklar</TabPanel>
+        <TabPanel value="7">Giderler</TabPanel>
+      </TabContext>
+    </Box>
+  );
 };
--- src/TabContexts/StudentTab.tsx
+++ src/TabContexts/StudentTab.tsx
@@ -1,106 +1,190 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
-import { useState } from "react";
-import dummyData from "../dummy.json";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from '@mui/material';
+import { useState } from 'react';
+import dummyData from '../dummy.json';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { DetailedStudent, Student } from '../Types';
 
-export const StudentTab = () => {
-  const [selectedStudent, setSelectedStudent] = useState<any>(null);
+const fieldLabels: Record<keyof DetailedStudent, string> = {
+  id: 'ID',
+  name: 'Ad',
+  surname: 'Soyad',
+  phoneNumber: 'Telefon Numarasi',
+  email: 'Email',
+  address: 'Adres',
+  birthDate: 'Dogum Tarihi',
+  isActive: 'Aktiflik Durumu',
+  registrationDate: 'Kayit Tarihi',
+  custodianId: 'Veli Id',
+};
+
+const initStudent: DetailedStudent = {
+  name: '',
+  surname: '',
+  id: '',
+  phoneNumber: '',
+  email: '',
+  address: '',
+  birthDate: '',
+  isActive: false,
+  registrationDate: '',
+  custodianId: '',
+};
+
+const StudentTab = () => {
+  const [selectedStudent, setSelectedStudent] = useState<DetailedStudent>({
+    name: '',
+    surname: '',
+    id: '',
+    phoneNumber: '',
+    email: '',
+    address: '',
+    birthDate: '',
+    isActive: false,
+    registrationDate: '',
+    custodianId: '',
+  });
   const [openUpdateDialog, setOpenUpdateDialog] = useState(false);
-  
-  const handleUpdateClick = (student: any) => {
-    setSelectedStudent(student);
+  const [students, setStudents] = useState<Student[]>(dummyData as Student[]);
+
+  const [openAddDialog, setOpenAddDialog] = useState(false);
+  const [newStudent, setNewStudent] = useState<DetailedStudent>(initStudent);
+
+  const handleUpdateClick = (student: Student) => {
+    const detailedStudent: DetailedStudent = dummyData.find(
+      (object) => object.id === student.id
+    ) as DetailedStudent;
+    setSelectedStudent(detailedStudent);
     setOpenUpdateDialog(true);
   };
-  
+
   const handleCloseUpdateDialog = () => {
     setOpenUpdateDialog(false);
   };
-  
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setSelectedStudent({ ...selectedStudent, [name]: value });
   };
-  
+
   const handleSaveChanges = () => {
     // Buradan backende kaydedicez yapılan değişiklikleri
-    console.log("Değişiklikler kaydedildi:", selectedStudent);
+    console.log('Değişiklikler kaydedildi:', selectedStudent);
+    setStudents((prevStudents) =>
+      prevStudents.map((student) =>
+        student.id === selectedStudent.id
+          ? { ...student, ...selectedStudent }
+          : student
+      )
+    );
     setOpenUpdateDialog(false);
   };
-  
+
+  const handleAddStudentClick = () => {
+    console.log(students);
+    setOpenAddDialog(true);
+  };
+
+  const handleAddStudentClose = () => {
+    setOpenAddDialog(false);
+  };
+
+  const handleAddStudentInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { name, value } = e.target;
+    setNewStudent({ ...newStudent, [name]: value });
+  };
+
+  const handleAddStudentSave = () => {
+    //backend e request atilicak
+    setStudents((prevStudends) => [...prevStudends, newStudent as Student]);
+    setNewStudent(initStudent);
+    setOpenAddDialog(false);
+  };
+
   const columns: GridColDef[] = [
-    { field: "isim", headerName: "İsim", width: 200 },
-    { field: "soyisim", headerName: "Soyisim", width: 200 },
-    { field: "id", headerName: "SSN", width: 200 },
-    { field: "telefon", headerName: "Telefon", width: 200 },
-    { field: "email", headerName: "E-Mail", width: 200 },
+    { field: 'id', headerName: 'Id', width: 200 },
+    { field: 'name', headerName: 'İsim', width: 200 },
+    { field: 'surname', headerName: 'Soyisim', width: 200 },
+    { field: 'phoneNumber', headerName: 'Telefon', width: 200 },
+    { field: 'email', headerName: 'E-Mail', width: 200 },
     {
-      field: "actions",
-      headerName: "İşlemler",
-      width: 120,
+      field: 'actions',
+      headerName: '',
+      width: 300,
       renderCell: (params) => {
         const handleEditClick = () => {
           handleUpdateClick(params.row);
         };
-  
-        return <Button onClick={handleEditClick}>Düzenle</Button>;
+
+        return (
+          <>
+            <Button onClick={handleEditClick}>Düzenle</Button>
+            <Button onClick={() => {}}>Detayli Bilgi</Button>
+            <Button onClick={() => {}} color="error">
+              Sil
+            </Button>
+          </>
+        );
       },
     },
   ];
-  
+
   return (
     <>
-      <DataGrid rows={dummyData} columns={columns} />
-  
+      <Button onClick={handleAddStudentClick}>Ekle</Button>
+      <Dialog open={openAddDialog} onClose={handleAddStudentClose}>
+        <DialogTitle>Öğrenci Ekleme</DialogTitle>
+        <DialogContent>
+          <>
+            {Object.keys(newStudent).map((key: string) => {
+              return (
+                <TextField
+                  key={key}
+                  label={fieldLabels[key as keyof DetailedStudent]}
+                  name={key}
+                  value={newStudent[key as keyof DetailedStudent]}
+                  onChange={handleAddStudentInputChange}
+                  fullWidth
+                  sx={{ marginTop: '16px' }}
+                />
+              );
+            })}
+          </>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseUpdateDialog}>İptal</Button>
+          <Button onClick={handleAddStudentSave}>Kaydet</Button>
+        </DialogActions>
+      </Dialog>
+
+      <DataGrid rows={students} columns={columns} />
       <Dialog open={openUpdateDialog} onClose={handleCloseUpdateDialog}>
         <DialogTitle>Öğrenci Düzenleme</DialogTitle>
         <DialogContent>
           {selectedStudent && (
             <>
-              <TextField
-                label="İsim"
-                name="isim"
-                value={selectedStudent.isim}
-                onChange={handleInputChange}
-                fullWidth
-				inputProps={{
-					readOnly: true,
-				}}
-				sx={{ marginTop: '16px' }} 
-              />
-              <TextField
-                label="Soyisim"
-                name="soyisim"
-                value={selectedStudent.soyisim}
-                onChange={handleInputChange}
-                fullWidth
-				inputProps={{
-					readOnly: true,
-				}}
-              />
-              <TextField
-                label="SSN"
-                name="id"
-                value={selectedStudent.id}
-                onChange={handleInputChange}
-                fullWidth
-				inputProps={{
-					readOnly: true,
-				}}
-              />
-              <TextField
-                label="Telefon"
-                name="telefon"
-                value={selectedStudent.telefon}
-                onChange={handleInputChange}
-                fullWidth
-              />
-              <TextField
-                label="E-Mail"
-                name="email"
-                value={selectedStudent.email}
-                onChange={handleInputChange}
-                fullWidth
-              />
+              {Object.keys(selectedStudent).map((key: string) => {
+                return (
+                  <TextField
+                    key={key}
+                    label={fieldLabels[key as keyof DetailedStudent]}
+                    name={key}
+                    value={selectedStudent[key as keyof DetailedStudent]}
+                    onChange={handleInputChange}
+                    disabled={key === 'id' || key === 'custodian_id'}
+                    fullWidth
+                    sx={{ marginTop: '16px' }}
+                  />
+                );
+              })}
             </>
           )}
         </DialogContent>
@@ -112,3 +196,5 @@ export const StudentTab = () => {
     </>
   );
 };
+
+export default StudentTab;
